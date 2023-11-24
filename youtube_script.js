96f949ca8bae29e51a0b0bbb7371215db03b1048
clinkinson <l.g.r.ramirez03@gmail.com>
2023-11-23 16:59:59 -0800

    Added youtube comments function

--- youtube_script.js
+++ youtube_script.js
@@ -1,3 +1,5 @@
+const apiKey = 'AIzaSyDvtNzdBCepDaWXlERreRS1HRl1vU_Z4mA';
+
 document.addEventListener('DOMContentLoaded', () => {
     const videoDetails = localStorage.getItem('selectedVideoDetails');
     
@@ -5,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const { videoId, title } = JSON.parse(videoDetails);
         document.getElementById('videoTitle').textContent = title;
         embedYouTubePlayer(videoId);
+        fetchYouTubeComments(videoId);
     } else {
         console.error('No video details found.');
     }
@@ -20,4 +23,35 @@ function embedYouTubePlayer(videoId) {
     iframe.allowFullscreen = true;
 
     playerContainer.appendChild(iframe);
+}
+function fetchYouTubeComments(videoId) {
+     const apiUrl = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&key=${apiKey}`;
+     // Make the API request
+     fetch(apiUrl)
+     .then(response => response.json())
+     .then(data => {
+         displayComments(data.items);
+     })
+     .catch(error => {
+         console.error('Error fetching comments:', error);
+     });
+ }
+
+function displayComments(comments) {
+    const commentsContainer = document.getElementById('commentsContainer');
+    // Clear existing content
+    commentsContainer.innerHTML = '';
+    // Iterate through comments and create HTML elements
+    comments.forEach(comment => {
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('comment');
+        const authorElement = document.createElement('p');
+        authorElement.textContent = comment.snippet.topLevelComment.snippet.authorDisplayName;
+        const textElement = document.createElement('p');
+        textElement.textContent = comment.snippet.topLevelComment.snippet.textDisplay;
+        commentElement.appendChild(authorElement);
+        commentElement.appendChild(textElement);
+        commentsContainer.appendChild(commentElement);
+    });
+}
 }
